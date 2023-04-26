3916f111157cf69bb6c43d87896504b9c2a3833c
Convitlacbay <convitlacbay@gmail.com>
2023-04-26 15:34:50 +0700

    done

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -5,14 +5,14 @@ import { useLocation } from "react-router-dom";
 import { useState } from "react";
 import { format } from "date-fns";
 import { DateRange } from "react-date-range";
-
+import SearchItem from "../../components/searchItem/SearchItem";
 
 const List = () => {
-    const { state } = useLocation();
-    const [destination, setDestination] = useState(state?.destination ?? "");
-    const [date, setDate] = useState(state?.date ?? {});
+    const location = useLocation();
+    const [destination, setDestination] = useState(location.state.destination);
+    const [date, setDate] = useState(location.state.date);
     const [openDate, setOpenDate] = useState(false);
-    const [options, setOptions] = useState(state?.options ?? []);
+    const [options, setOptions] = useState(location.state.options);
 
     return (
         <div>
@@ -28,12 +28,10 @@ const List = () => {
                         </div>
                         <div className="lsItem">
                             <label>Check-in Date</label>
-                            {date[0].startDate && date[0].endDate &&
-                                <span onClick={() => setOpenDate(!openDate)}>
-                                    {format(date[0].startDate, 'MM/dd/yyyy')}
-                                    to {format(date[0].endDate, 'MM/dd/yyyy')}
-                                </span>
-                            }
+                            <span onClick={() => setOpenDate(!openDate)}>{`${format(
+                                date[0].startDate,
+                                "MM/dd/yyyy"
+                            )} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
                             {openDate && (
                                 <DateRange
                                     onChange={(item) => setDate([item.selection])}
@@ -42,14 +40,67 @@ const List = () => {
                                 />
                             )}
                         </div>
+                        <div className="lsItem">
+                            <label>Options</label>
+                            <div className="lsOptions">
+                                <div className="lsOptionItem">
+                                    <span className="lsOptionText">
+                                        Min price <small>per night</small>
+                                    </span>
+                                    <input type="number" className="lsOptionInput" />
+                                </div>
+                                <div className="lsOptionItem">
+                                    <span className="lsOptionText">
+                                        Max price <small>per night</small>
+                                    </span>
+                                    <input type="number" className="lsOptionInput" />
+                                </div>
+                                <div className="lsOptionItem">
+                                    <span className="lsOptionText">Adult</span>
+                                    <input
+                                        type="number"
+                                        min={1}
+                                        className="lsOptionInput"
+                                        placeholder={options.adult}
+                                    />
+                                </div>
+                                <div className="lsOptionItem">
+                                    <span className="lsOptionText">Children</span>
+                                    <input
+                                        type="number"
+                                        min={0}
+                                        className="lsOptionInput"
+                                        placeholder={options.children}
+                                    />
+                                </div>
+                                <div className="lsOptionItem">
+                                    <span className="lsOptionText">Room</span>
+                                    <input
+                                        type="number"
+                                        min={1}
+                                        className="lsOptionInput"
+                                        placeholder={options.room}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <button>Search</button>
                     </div>
                     <div className="listResult">
-
+                        <SearchItem />
+                        <SearchItem />
+                        <SearchItem />
+                        <SearchItem />
+                        <SearchItem />
+                        <SearchItem />
+                        <SearchItem />
+                        <SearchItem />
+                        <SearchItem />
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default List
+export default List;
